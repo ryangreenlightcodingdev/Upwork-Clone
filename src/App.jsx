05b7338627bb2ba6816312dec38a03f905163de7
rvandykenine15 <141283938+rvandykenine15@users.noreply.github.com>
2023-12-16 02:42:53 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { Routes, Route, Link } from "react-router-dom";
+// import Login from "./Login";
+// import JobBoard from "./JobBoard";
+// import JobDetail from "./JobDetail";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/jobs" element={<JobBoard />} />
+        <Route path="/job/:id" element={<JobDetail />} />
+      </Routes> */}
+      test
+    </div>
+  );
+}
